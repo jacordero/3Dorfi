214252fb4534980f8ed6921274be393773513749
Prince Songwa <princeulrich2@yahoo.fr>
2018-05-13 18:02:38 +0200

    unversioned files

---
+++ ObjectRecognizer/src/nl/tue/vc/projection/ProjectionMatrices.java
@@ -0,0 +1,43 @@
+package nl.tue.vc.projection;
+
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfDouble;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfPoint3f;
+
+public class ProjectionMatrices {
+
+	private Mat rotationVector;
+	
+	private Mat translationVector;
+	
+	private Mat intrinsicParameters;
+	
+	private MatOfDouble distorsionCoefficients;
+	
+	public ProjectionMatrices(Mat rotationVector, Mat translationVector, 
+			Mat intrinsicParameters, MatOfDouble distorsionCoefficients) {
+		this.rotationVector = rotationVector;
+		this.translationVector = translationVector;
+		this.intrinsicParameters = intrinsicParameters;
+		this.distorsionCoefficients = distorsionCoefficients;
+	}
+	
+	public Mat getRotationVector(){
+		return rotationVector;
+	}
+	
+	public Mat getTranslationVector(){
+		return translationVector;
+	}
+	
+	public Mat getIntrinsicParameters(){
+		return intrinsicParameters;
+	}
+	
+	public MatOfDouble getDistorsionCoefficients(){
+		return distorsionCoefficients;
+	}
+	
+}
